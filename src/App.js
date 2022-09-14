074d160fac67bb87ebc3f6754cedaf0d6ba81692
mac <shrinkhla.rajpoot@revirt.space>
2022-09-14 14:13:33 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import { useState } from 'react';
 import './App.css';
 import BasicRoutes from './routes/basic-routes';
 function App() {
-  // const [ id,setId] = useState("")
-  return (
+return (
    <BasicRoutes/>
   );
 }
--- src/containers/Chat.js
+++ src/containers/Chat.js
@@ -7,15 +7,19 @@ import { useUserData } from '../store/slice/globalStates';
 import { user } from '../utils/userDetails';
 export const RevirtChat = () =>{
     const [client,setClient] = useState(null)
-    const id = useStore1(state=>state.userId)
+    const user = {
+      id:useStore1(state=>state.userId)
+    }
+     
+ 
     useEffect(()=>{
       async function init(){
       const chatClient = StreamChat.getInstance(process.env.REACT_APP_API_KEY);
-      await chatClient.connectUser(user,chatClient.devToken(id))
-      const filters = { type: 'messaging', members: { $in: [id] } }
+      await chatClient.connectUser(user,chatClient.devToken(user.id))
+      const filters = { type: 'messaging', members: { $in: [user.id] } }
       const channel = await chatClient.queryChannels(filters);
       channel[0].on('message.new', (e) => {
-      if(e.message.user.id == user.id) return;
+      if(e.message.id== user.id) return;
       const audio = new Audio(process.env.REACT_APP_TEST_AUDIO_URL);
       audio.play();})
     setClient(chatClient)
@@ -25,7 +29,7 @@ export const RevirtChat = () =>{
     if(client) return ()=>client.disconnectUser()
     },[])
     
-    const filters = { type: 'messaging', members: { $in: [id] } }
+    const filters = { type: 'messaging', members: { $in: [user.id] } }
      if(!client) return <LoadingIndicator/>
     return(
         <Chat client={client}  darkMode={darkModeTheme}>
